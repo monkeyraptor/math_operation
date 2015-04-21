b6d8eca7ffb7c1466f924c8c3009f2f615c9fd5e
Monkey Raptor <paul.johanov@gmail.com>
2015-04-21 09:00:14 +0700

    Update custom_math.js

--- custom_math.js
+++ custom_math.js
@@ -6,28 +6,31 @@
 var math = {
     //checking above 21 digit length or infinity
     check: function (a) {
+        "use strict";
         var result = 0;
-        
+
         if (a.toString().length > 21 || !isFinite(a)) {
             result = 1;
         }
         return result;
     },
-    
+
     //rounding down
     rd: function (a) {
+        "use strict";
         return Math.floor(a);
     },
-    
+
     //*****************************************
     //[1] Factorial
     factorial: function (n) {
-        var i = 1, 
+        "use strict";
+        var i = 1,
             result = 1;
-            
+
         if (Number(n) >= 0) {
             n = this.rd(n);
-            
+
             if (n > 1) {
                 for (i; i <= n; i++) {
                     result *= i;
@@ -43,27 +46,28 @@ var math = {
         }
         return result;
     },
-    
+
     //*****************************************
     //[2] Combination
     combination: function (n, k) {
-        var start, 
-            numerator = 1, 
-            denumerator = 1, 
+        "use strict";
+        var start,
+            numerator = 1,
+            denumerator = 1,
             i = 1,
             result;
-            
+
         if (n >= k && Number(n) >= 0 && Number(k) >= 0) {
             n = this.rd(n);
             k = this.rd(k);
-            
+
             if (n === k || k === 0) {
                 result = 1;
             } else if (k === 1) {
                 result = n;
             } else {
                 start = n - k + 1;
-                
+
                 for (i; i <= k; i++) {
                     denumerator *= i;
                 }
@@ -80,18 +84,19 @@ var math = {
         }
         return result;
     },
-    
+
     //*****************************************
     //[3] Permutation
     permutation: function (n, k) {
-        var start, 
+        "use strict";
+        var start,
             numerator = 1,
             result;
-            
+
         if (n >= k && Number(n) >= 0 && Number(k) >= 0) {
             n = this.rd(n);
             k = this.rd(k);
-            
+
             if (n === k || k === 0) {
                 result = 1;
             } else if (k === 1) {
@@ -111,51 +116,52 @@ var math = {
         }
         return result;
     },
-    
+
     //*****************************************
     //[4] Adding comma to large integer
     addcomma: function (your_number) {
+        "use strict";
         var f = [],
-            p, 
-            r, 
-            i = 1;            
-          
-        if (!isNaN(your_number)){
-            if (your_number < 0) { p = "-"; }
-            else { p = ""; }   
-        
+            p,
+            r,
+            i = 1;
+
+        if (!isNaN(your_number)) {
+            if (your_number < 0) { p = "-"; } else { p = ""; }
+
             r = this.rd(Math.abs(your_number)).toString();
             f = r.split("").reverse();
-        
-            for (i; i<= Math.floor(f.length/4); i++) {
+
+            for (i; i <= this.rd(f.length / 4); i++) {
                 f.splice((4 * i - 1), 0, ",");
             }
             your_number = p + f.reverse().join("");
         } else {
             your_number = "INPUT ERROR: must be number";
         }
-      return your_number;
+        return your_number;
     },
-    
+
     //*****************************************
     //[5] Generating Fibonacci sequence
-    fibonacci: function(first, second, how_many) {
-        var i = 2,            
-            result,            
+    fibonacci: function (first, second, how_many) {
+        "use strict";
+        var i = 2,
+            result,
             array; //starting numbers
-        
+
         if (Number(first) !== 0 && Number(second) !== 0 && Number(how_many) !== 0) {
             first = this.rd(first);
             second = this.rd(second);
             how_many = this.rd(how_many);
             array = [first, second];
-            
+
             for (i = 2; i <= how_many + 1; i++) {
                 array.push(array[i - 1] + array[i - 2]);
             }
             result = array.join(", ");
         } else {
-          result = "INPUT ERROR: only accepts number and non-zero";
+            result = "INPUT ERROR: only accepts number and non-zero";
         }
         return result;
     }
