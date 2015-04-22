3911bb8f4ae0c35db5bdee66e2718e1125d017ae
Monkey Raptor <paul.johanov@gmail.com>
2015-04-23 00:30:48 +0700

    Line 170
    !isNaN()

--- custom_math.js
+++ custom_math.js
@@ -167,7 +167,7 @@ var math = {
             result,
             array; //starting numbers
 
-        if (!!Number(first) && !!Number(second) && Number(how_many) > 0) { //allows 1st and 2nd numbers to be 0, but not *how_many*
+        if (!isNaN(first) && !isNaN(second) && Number(how_many) > 0) { //allows 1st and 2nd numbers to be 0, but not *how_many*
             first = this.rd(first);
             second = this.rd(second);
             how_many = this.rd(how_many);
