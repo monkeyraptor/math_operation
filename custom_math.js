d487df37f3f68c876026b02d0a476c00c1bd3503
Monkey Raptor <paul.johanov@gmail.com>
2015-09-02 23:09:19 +0700

    Update custom_math.js

--- custom_math.js
+++ custom_math.js
@@ -242,7 +242,7 @@ math = {
         i = 2;
         a = Number(a);
         //maximum digit length = 20 (999,999,999,999,999,999,999)
-        if (a.toString().length <= 21) {
+        if (a.toString().length < 21) {
             if (a > 1) {
                 a = Math.floor(a);
                 limit = Math.floor(Math.sqrt(a));
@@ -261,7 +261,7 @@ math = {
             }
 
         } else {
-            result = "INPUT ERROR: digit length <= 21";
+            result = "INPUT ERROR: digit length < 21";
         }
 
         return result;
@@ -489,7 +489,7 @@ misc_math = {
         return obj;
     },
 
-    //checking equal or above 21 digit length or infinity
+    //checking the compacted digit or infinity
     check: function (a) {
         "use strict";
         var result = 0;
