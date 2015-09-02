86ea7e8ecd2f2c315d750f155704eb2068d5d97a
Monkey Raptor <paul.johanov@gmail.com>
2015-09-02 14:00:52 +0700

    Update and rename custom_math_2.js to custom_math.js

--- demo/custom_math.js
+++ demo/custom_math.js
@@ -1,17 +1,5 @@
-/*! READTHIS: https://github.com/monkeyraptor/math_operation 
- *
- *  This has no *this* keyword for inner object function invocation.
- *  They're all using absolute reference.
- *
- *  Change "math" global keyword if it collided with your own JavaScript thingy.
- *
- *  There are "math" and "misc_math" variables (all lowercase).
- * 
- *  For example:
- *  switch "math" and "misc_math" into "goat" and "misc_goat", respectively. ALL OF THEM.
- *  Then to call the *factorial* function: goat.factorial(2).
- */
-
+// READTHIS: https://github.com/monkeyraptor/math_operation
+// Version 2015-09-02
 // GLOBAL VARIABLES
 var math, misc_math;
 
@@ -21,13 +9,14 @@ math = {
     //[1] Factorial
     factorial: function (n) {
         "use strict";
-        var i = 1,
-            result = 1;
-
-        if (!isNaN(n) && n > -1) {
-            n = misc_math.rd(n);
+        var i, result;
+        i = 1;
+        result = 1;
+        n = Number(n);
+        if (n >= 0) {
+            n = Math.floor(n);
             if (n > 1) {
-                while (i <= n) {                    
+                while (i <= n) {
                     result *= i;
                     i += 1;
                 }
@@ -45,15 +34,15 @@ math = {
     //[2] Combination
     combination: function (n, k) {
         "use strict";
-        var start,
-            numerator = 1,
-            denumerator = 1,
-            i = 1,
-            result;
-
+        var start, numerator, denumerator, i, result;
+        numerator = 1;
+        denumerator = 1;
+        i = 1;
+        n = Number(n);
+        k = Number(k);
         if (n - k >= 0 && n >= 0 && k >= 0) {
-            n = misc_math.rd(n);
-            k = misc_math.rd(k);
+            n = Math.floor(n);
+            k = Math.floor(k);
 
             if (n === k) {
                 result = 1;
@@ -70,7 +59,7 @@ math = {
                     numerator *= start;
                     start += 1;
                 }
-                result = misc_math.rd(numerator / denumerator);
+                result = Math.floor(numerator / denumerator);
             }
         } else {
             result = "INPUT ERROR: n >= k, n >= 0, k >= 0";
@@ -85,13 +74,13 @@ math = {
     //[3] Permutation
     permutation: function (n, k) {
         "use strict";
-        var start,
-            numerator = 1,
-            result;
-
+        var start, numerator, result;
+        numerator = 1;
+        n = Number(n);
+        k = Number(k);
         if (n - k >= 0 && n >= 0 && k >= 0) {
-            n = misc_math.rd(n);
-            k = misc_math.rd(k);
+            n = Math.floor(n);
+            k = Math.floor(k);
             if (n === 0 || k === 0) {
                 result = 1;
             } else if (n === k) {
@@ -119,28 +108,27 @@ math = {
     //[4] Adding comma to large integer
     addcomma: function (your_number) {
         "use strict";
-        var f = [],
-            p,
-            r,
-            i = 1;
-
-        if (!isNaN(your_number)) {
+        var f, p, r, i;
+        f = [];
+        i = 1;
+        your_number = Number(your_number);
+        if (!misc_math.check(your_number)) {
             if (your_number < 0) {
                 p = "-";
             } else {
                 p = "";
             }
 
-            r = misc_math.ab(misc_math.rd(your_number)).toString();
+            r = Math.abs(Math.floor(your_number)).toString();
             f = r.split("").reverse();
 
-            while (i <= misc_math.rd(f.length / 4)) {
+            while (i <= Math.floor(f.length / 4)) {
                 f.splice((4 * i - 1), 0, ",");
                 i += 1;
             }
             your_number = p + f.reverse().join("");
         } else {
-            your_number = "INPUT ERROR: must be number";
+            your_number = "INPUT ERROR: must be number or too large";
         }
         return your_number;
     },
@@ -149,14 +137,15 @@ math = {
     //[5] Generating Fibonacci sequence
     fibonacci: function (first, second, how_many) {
         "use strict";
-        var i = 2,
-            result,
-            array; //starting numbers
-
-        if (!isNaN(first) && !isNaN(second) && Number(how_many) > 0) {
-            first = misc_math.rd(first);
-            second = misc_math.rd(second);
-            how_many = misc_math.rd(how_many);
+        var i, result, array;
+        i = 2;
+        first = Number(first);
+        second = Number(second);
+        how_many = Number(how_many);
+        if (!!first && !!second && how_many > 0) {
+            first = Math.floor(first);
+            second = Math.floor(second);
+            how_many = Math.floor(how_many);
             array = [first, second];
 
             while (i <= how_many + 1) {
@@ -174,19 +163,18 @@ math = {
     //[6] Get the factor of a positive integer
     factorof: function (a, b) {
         "use strict";
-        var result,
-            i = 1,
-            j = 0,
-            k = 0,
-            array = [],
-            left = [],
-            right = [],
-            a_l,
-            divider;
-
-        if (Number(a) !== 0) { //check if number and non-zero
+        var result, i, j, k, array, left, right, a_l, divider;
+        i = 1;
+        j = 0;
+        k = 0;
+        array = [];
+        left = [];
+        right = [];
+        a = Number(a);
+        b = Number(b);
+        if (a !== 0) { //check if number and non-zero
             if (a < 0) { //convert it to positive integer and round down
-                a = misc_math.ab(misc_math.rd(a));
+                a = Math.abs(Math.floor(a));
             }
             while (i <= a) {
                 if (a % i === 0) {
@@ -215,7 +203,7 @@ math = {
                         k += 1;
                     }
                 } else { //for odd array length
-                    divider = misc_math.rd(a_l / 2);
+                    divider = Math.floor(a_l / 2);
 
                     while (j < divider) {
                         left.push(array[j]);
@@ -250,15 +238,14 @@ math = {
     //[7] Checking prime number
     prime_check: function (a) {
         "use strict";
-        var result,
-            i = 2,
-            limit;
-
-        //maximum digit length = 21 (999,999,999,999,999,999,999)
-        if (a.toString().length < 21) {
-            if (!isNaN(a) && a > 1) {
-                a = misc_math.rd(a);
-                limit = misc_math.rd(misc_math.sqr(a));
+        var result, i, limit;
+        i = 2;
+        a = Number(a);
+        //maximum digit length = 20 (999,999,999,999,999,999,999)
+        if (a.toString().length <= 21) {
+            if (a > 1) {
+                a = Math.floor(a);
+                limit = Math.floor(Math.sqrt(a));
                 result = "PRIME number";
 
                 while (i <= limit) {
@@ -284,12 +271,12 @@ math = {
     //[8] Generate prime numbers
     generate_prime: function (start, how_many) {
         "use strict";
-        var result,
-            primes = [];
-
-        if (Number(start) > 1 && Number(how_many) > 0) {
-            start = misc_math.rd(start);
-            how_many = misc_math.rd(how_many);
+        var result, primes = [];
+        start = Number(start);
+        how_many = Number(how_many);
+        if (start > 1 && how_many > 0) {
+            start = Math.floor(start);
+            how_many = Math.floor(how_many);
 
             while (how_many > 0) {
                 if (!math.prime_check(start).match(/not/gi)) {
@@ -311,15 +298,14 @@ math = {
     //[9] Generate Pascal's triangle sequence (one particular line or many lines)
     pascal_triangle: function (a, b) {
         "use strict";
-        var array = [],
-            i = 0,
-            j,
-            k = 0,
-            result = {},
-            store = [];
-
-        //SPLENDID arithmetic method from http://www.ywhmaths.webs.com/Arithmetic/Pascal.html
+        var array, i, j, k, result, store;
+        array = [];
+        i = 0;
+        k = 0;
+        result = {};
+        store = [];
         function pt(n, r) { //pt as in *Pascal's Triangle*
+            //SPLENDID arithmetic method from http://www.ywhmaths.webs.com/Arithmetic/Pascal.html
             var buffer;
             if (n > 1 && r > 0 && r < n) {
                 buffer = pt(n - 1, r - 1) + pt(n - 1, r);
@@ -328,9 +314,10 @@ math = {
             }
             return buffer;
         }
-
-        if (Number(a) > 0) {
-            a = misc_math.rd(a); //round down
+        a = Number(a);
+        b = Number(b);
+        if (a > 0) {
+            a = Math.floor(a);
 
             while (i < a) {
                 j = 0;
@@ -342,10 +329,10 @@ math = {
                 result[i] = array; //fill the *result* object with the *array*
                 i += 1;
             }
-            if (Number(b) === 0) { //only a particular line
+            if (b === 0) { //only a particular line
                 result = result[a - 1].join(", "); //array to string
 
-            } else if (Number(b) === 1) { //show all lines until the limit number given
+            } else if (b === 1) { //show all lines until the limit number given
                 while (k < a) {
                     store.push(result[k].join(" ")); //this can be changed to fit your layout.
                     k += 1;
@@ -367,19 +354,18 @@ math = {
     //[10] Least common multiple (LCM)
     lcm: function (a, b, c) { //min 2 inputs, max of 3
         "use strict";
-        var gcf,
-            result,
-            t,
-            u,
-            v = [],
-            z = [];
-
-        if (isNaN(c)) { //only 2 inputs provided
+        var gcf, result, t, u, v, z;
+        v = [];
+        z = [];
+        a = Number(a);
+        b = Number(b);
+        c = Number(c);
+        if (!c) { //only 2 inputs provided
             if (a > 0 && b > 0) {
-                a = misc_math.rd(a);
-                b = misc_math.rd(b);
+                a = Math.floor(a);
+                b = Math.floor(b);
                 v = [a, b];
-                z = v.sort(misc_math.sorthl);
+                z = v.sort(misc_math.hl);
                 t = z[0] % z[1];
 
                 if (t === 0) {
@@ -395,23 +381,14 @@ math = {
 
         } else { //3rd input is provided
             if (a > 0 && b > 0 && c > 0) {
-                a = misc_math.rd(a);
-                b = misc_math.rd(b);
-                c = misc_math.rd(c);
+                a = Math.floor(a);
+                b = Math.floor(b);
+                c = Math.floor(c);
                 v = [a, b, c];
-                z = v.sort(misc_math.sorthl);
-
-                if (z[0] % z[2] === 0) {
-                    result = math.lcm(z[0], z[1], undefined);
-
-                } else if (z[0] % z[1] === 0) {
-                    result = math.lcm(z[0], z[2], undefined);
-
-                } else {
-                    t = math.lcm(z[0], z[1], undefined);
-                    u = math.lcm(t, z[2], undefined);
-                    result = math.lcm(t, u, undefined);
-                }
+                z = v.sort(misc_math.hl);
+                t = math.lcm(z[0], z[1], undefined);
+                u = math.lcm(t, z[2], undefined);
+                result = math.lcm(t, u, undefined);
             } else {
                 result = "INPUT ERROR: inputs must be natural number (N*)";
             }
@@ -424,39 +401,36 @@ math = {
     //[11] Greatest common factor (GCF)
     gcf: function (a, b, c) { //min 2 inputs, max of 3
         "use strict";
-        var num_1,
-            num_2,
-            num_3,
-            result;
-
-        if (isNaN(c)) { //3rd input undefined
+        var num_1, num_2, num_3, result;
+        a = Number(a);
+        b = Number(b);
+        c = Number(c);
+        if (!c) { //3rd input undefined
             if (a > 0 && b > 0) {
-                a = misc_math.rd(a);
-                b = misc_math.rd(b);
+                a = Math.floor(a);
+                b = Math.floor(b);
 
                 //get factors - convert string to array
                 num_1 = math.factorof(a, 0).split(", ");
                 num_2 = math.factorof(b, 0).split(", ");
-
                 //put result
                 result = misc_math.c_a(num_1, num_2).same_elements
-                        .sort(misc_math.sorthl)[0];
+                    .sort(misc_math.hl)[0];
             } else {
                 result = "INPUT ERROR: min 2 inputs and natural number (N*)";
             }
-        } else if (Number(c) > 0) { //3rd input is provided
-            a = misc_math.rd(a);
-            b = misc_math.rd(b);
-            c = misc_math.rd(c);
+        } else if (c > 0) { //3rd input is provided
+            a = Math.floor(a);
+            b = Math.floor(b);
+            c = Math.floor(c);
 
             //get factors - convert string to array
             num_1 = math.factorof(a, 0).split(", ");
             num_2 = math.factorof(b, 0).split(", ");
             num_3 = math.factorof(c, 0).split(", ");
-
             //put result
             result = misc_math.c_a(misc_math.c_a(num_1, num_2).same_elements, num_3)
-                    .same_elements.sort(misc_math.sorthl)[0];
+                .same_elements.sort(misc_math.hl)[0];
         } else {
             result = "INPUT ERROR: inputs must be natural number (N*)";
         }
@@ -468,62 +442,49 @@ math = {
 /***************************************************************************/
 // "misc_math" variable definitions
 misc_math = {
-    //compare two arrays
-    c_a: function (a, b) { //https://github.com/monkeyraptor/compare_2_arrays
+    // compare two arrays
+    // https://github.com/monkeyraptor/compare_2_arrays/blob/master/compare_arrays_3.js
+    c_a: function (a, b) {
         "use strict";
-        var c,
-            d,
-            l_0,
-            l_1,
-            obj = {};
-        function trim(a) {
-            var i = 0,
-                l = a.length,
-                buffer = [];
-            while (i < l) {
-                if (!a[i].toString().match("(deleted)")) {
-                    buffer.push(a[i]);
-                }
-                i += 1;
-            }
-            return buffer;
-        }
-        function splice_it(k, l, m, n) {
-            var i = 0,
-                j,
-                buffer = [];
-            while (i < m) {
-                j = 0;
-                while (j < n) {
-                    if (l[j] === k[i]) {
-                        buffer.push(l[j]);
-                        l.splice(j, 1, l[j] + " (deleted)");
-                        k.splice(i, 1, l[i] + " (deleted)");
+        var c, d, obj;
+        obj = {
+            difference: [],
+            same_elements: []
+        };
+        function process_it(k, l) {
+            var same = [];
+            function trim(a) {
+                var i = a.length;
+                while (i >= 0) {
+                    if (a[i] === "_*deleted*_") {
+                        a.splice(i, 1);
                     }
-                    j += 1;
+                    i -= 1;
                 }
-                i += 1;
+                return a;
             }
+            l.forEach(function (v, i) {
+                k.forEach(function (vv, ii) {
+                    if (v === vv) {
+                        same.push(vv);
+                        l.splice(i, 1, "_*deleted*_");
+                        k.splice(ii, 1, "_*deleted*_");
+                    }
+                });
+            });
             return {
-                "difference": trim(k).concat(trim(l)),
-                "same_elements": buffer
+                difference: trim(l.concat(k)),
+                same_elements: same
             };
         }
-        if (typeof a === "object" && typeof b === "object") {
+        if (a instanceof Array && b instanceof Array) {
             c = a.slice();
             d = b.slice();
-            l_0 = c.length;
-            l_1 = d.length;
             if (c.length >= d.length) {
-                obj = splice_it(c, d, l_0, l_1);
+                obj = process_it(c, d);
             } else {
-                obj = splice_it(d, c, l_1, l_0);
+                obj = process_it(d, c);
             }
-        } else {
-            obj = {
-                "difference": [],
-                "same_elements": []
-            };
         }
         return obj;
     },
@@ -532,38 +493,20 @@ misc_math = {
     check: function (a) {
         "use strict";
         var result = 0;
-        if (a.toString().length >= 21 || !isFinite(a) || a % 1 > 0) {
+        if (/e\+/i.test(a.toString()) || !isFinite(a)) {
             result = 1;
         }
         return result;
     },
 
-    //rounding down
-    rd: function (a) {
-        "use strict";
-        return Math.floor(a);
-    },
-
-    //absolute
-    ab: function (a) {
-        "use strict";
-        return Math.abs(a);
-    },
-
-    //square root
-    sqr: function (a) {
-        "use strict";
-        return Math.sqrt(a);
-    },
-
     //HIGH to low (numeral) sorter
-    sorthl: function (a, b) {
+    hl: function (a, b) {
         "use strict";
         return parseInt(b, 10) - parseInt(a, 10);
     },
 
     //LOW to high (numeral) sorter
-    sortlh: function (a, b) {
+    lh: function (a, b) {
         "use strict";
         return parseInt(a, 10) - parseInt(b, 10);
     }
